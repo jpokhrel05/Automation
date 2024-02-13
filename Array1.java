5c1cbea66f0e67124dd0d72f7789f60f851bb09e
jpokhrel05 <159857965+jpokhrel05@users.noreply.github.com>
2024-02-13 18:36:01 -0500

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int a[] = {10,20,15,5,50};
+		for(int i =0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
---
+++ Array2.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		int arr[][] = {{10,5,2},{14,15,6},{17,21,13}};
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
